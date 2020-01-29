3e9deef447b29e5e7bb9b618030cc61078bc32ea
intrening <wol87@ya.ru>
2020-01-29 09:34:17 +0300

    update echo

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
-import logging, os, telegram
+import logging
+import os
+import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
@@ -7,6 +9,7 @@ DEBUG_BOT_TOKEN = os.environ['DEBUG_BOT_TOKEN']
 CHAT_ID = os.environ['CHAT_ID']
 
 logger = logging.getLogger("dvmn_bot")
+
 
 class MyLogsHandler(logging.Handler):
     def __init__(self, bot_token, chat_id, *args, **kwargs):
@@ -17,6 +20,7 @@ class MyLogsHandler(logging.Handler):
     def emit(self, record):
         log_entry = self.format(record)
         self.debug_bot.send_message(self.chat_id, text=log_entry)
+
 
 def start(bot, update):
     """Send a message when the command /start is issued."""
@@ -38,50 +42,34 @@ def error(bot, update, error):
     global logger
     logger.warning('Update "%s" caused error "%s"', update, error)
 
+
 def main():
     global logger
     logger.setLevel(logging.INFO)
     logger.addHandler(
-        MyLogsHandler(bot_token=os.environ['DEBUG_BOT_TOKEN'],
-        chat_id=CHAT_ID)
+        MyLogsHandler(
+            bot_token=os.environ['DEBUG_BOT_TOKEN'],
+            chat_id=CHAT_ID,
+        ),
     )
 
-    bot = telegram.Bot(token=GAMEVERB_BOT_TOKEN)
+    # bot = telegram.Bot(token=GAMEVERB_BOT_TOKEN)
+
+    updater = Updater(GAMEVERB_BOT_TOKEN)
+    dispatcher = updater.dispatcher
     logger.info('Бот GAMEVERB_BOT запущен')
 
-    # Create the EventHandler and pass it your bot's token.
-    updater = Updater(GAMEVERB_BOT_TOKEN)
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
 
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
+    echo_handler = MessageHandler(Filters.text, echo)
+    dispatcher.add_handler(echo_handler)
 
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
+    dispatcher.add_error_handler(error)
 
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
-    # log all errors
-    dp.add_error_handler(error)
-
-    # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
-    # while True:
-    #     try:
-    #         text = 'eer'
-    #         bot.send_message(chat_id=CHAT_ID, text=text)
-    #     except requests.exceptions.ReadTimeout:
-    #         pass
-    #     except requests.exceptions.ConnectionError:
-    #         time.sleep(10)
-    #     except Exception as err:
-    #         logger.error(err, exc_info=True)
 
 if __name__ == "__main__":
     main()
