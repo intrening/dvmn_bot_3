7a94931a28d7110dd6487e922cee42256b203bac
intrening <wol87@ya.ru>
2020-01-29 10:15:59 +0300

    add dialogflow

--- bot.py
+++ bot.py
@@ -2,11 +2,12 @@ import logging
 import os
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
+import dialogflow_v2 as dialogflow
 
 GAMEVERB_BOT_TOKEN = os.environ['GAMEVERB_BOT_TOKEN']
 DEBUG_BOT_TOKEN = os.environ['DEBUG_BOT_TOKEN']
 CHAT_ID = os.environ['CHAT_ID']
+PROJECT_ID = os.environ['PROJECT_ID']
 
 logger = logging.getLogger("dvmn_bot")
 
@@ -27,14 +28,39 @@ def start(bot, update):
     update.message.reply_text('Здравствуйте!')
 
 
-def help(bot, update):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+def detect_intent_texts(project_id, session_id, text, language_code='ru-RU'):
+    """Returns the result of detect intent with text as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.types.TextInput(
+        text=text, language_code=language_code)
+
+    query_input = dialogflow.types.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        session=session, query_input=query_input)
+
+    return (response.query_result.fulfillment_text)
 
 
 def echo(bot, update):
     """Echo the user message."""
     update.message.reply_text(update.message.text)
+
+
+def take_dialogflow_answer(bot, update):
+    """Echo the user message."""
+    answer = detect_intent_texts(
+        project_id=PROJECT_ID,
+        session_id=update.message.chat_id,
+        text=update.message.text,
+    )
+    update.message.reply_text(answer)
 
 
 def error(bot, update, error):
@@ -53,8 +79,6 @@ def main():
         ),
     )
 
-    # bot = telegram.Bot(token=GAMEVERB_BOT_TOKEN)
-
     updater = Updater(GAMEVERB_BOT_TOKEN)
     dispatcher = updater.dispatcher
     logger.info('Бот GAMEVERB_BOT запущен')
@@ -62,8 +86,8 @@ def main():
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
 
-    echo_handler = MessageHandler(Filters.text, echo)
-    dispatcher.add_handler(echo_handler)
+    answer_handler = MessageHandler(Filters.text, take_dialogflow_answer)
+    dispatcher.add_handler(answer_handler)
 
     dispatcher.add_error_handler(error)
 
